67244b54334524f91cc2e645a9c99aa31912b24d
Dante Paniagua <dantep@smartfran.com>
2022-09-30 17:04:18 -0300

    Corrijo comillas

--- powershell/installPowerShell51/installer.sh
+++ powershell/installPowerShell51/installer.sh
@@ -1,17 +1,20 @@
-VMNAME='sfcgvm11'
-RESGROUP='DEFAULTGROUP01'
-URI_DOWNLOAD='https://raw.githubusercontent.com/smartit-ar/iaasSmartFran/main/powershell/installPowerShell51/installPowershell51.ps1'
-URI_DW_PATH='C:\Temp\installPowershell51.ps1'
-URI_PRESET='[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12'
+#!/bin/bash
+{
+	VMNAME='sfcgvm12'
+	RESGROUP='DEFAULTGROUP01'
+	URI_DOWNLOAD='https://raw.githubusercontent.com/smartit-ar/iaasSmartFran/main/powershell/installPowerShell51/installPowershell51.ps1'
+	URI_DW_PATH='C:\Temp\installPowershell51.ps1'
+	URI_PRESET='[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12'
 
-az vm run-command invoke \
-	--resource-group $RESGROUP \
-	--name $VMNAME --command-id 'RunPowerShellScript' \
-	--scripts '"$URI_PRESET"; Invoke-WebRequest -Uri "$URI_DOWNLOAD" -OutFile "$URI_DW_PATH"' \
-	--query 'value[*].message'
-	
-az vm run-command invoke \
-	--resource-group $RESGROUP \
-	--name $VMNAME --command-id 'RunPowerShellScript' \
-	--scripts 'powershell.exe "$URI_DW_PATH"' \
-	--query 'value[*].message'
+	az vm run-command invoke \
+		--resource-group $RESGROUP \
+		--name $VMNAME --command-id 'RunPowerShellScript' \
+		--scripts '$URI_PRESET; Invoke-WebRequest -Uri $URI_DOWNLOAD -OutFile $URI_DW_PATH' \
+		--query 'value[*].message'
+		
+	az vm run-command invoke \
+		--resource-group $RESGROUP \
+		--name $VMNAME --command-id 'RunPowerShellScript' \
+		--scripts 'powershell.exe $URI_DW_PATH' \
+		--query 'value[*].message'
+}
